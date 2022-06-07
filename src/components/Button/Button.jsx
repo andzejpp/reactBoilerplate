045e781501b8a9ca6fea7b5e4fc5bceb29133bb4
Petras Slekys <petras@ibm.com>
2022-06-07 20:58:18 +0300

    added PropTypes to components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import * as S from "./Button.styles";
 
-const Button = ({ children }) => {
-  return <S.Button>{children}</S.Button>;
+const Button = ({ type, children, handleClick }) => {
+  return (
+    <S.Button type={type} onClick={handleClick}>
+      {children}
+    </S.Button>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf(["submit", "reset", "button"]),
+  children: PropTypes.string.isRequired,
+  handleClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  type: "button",
 };
 
 export default Button;
